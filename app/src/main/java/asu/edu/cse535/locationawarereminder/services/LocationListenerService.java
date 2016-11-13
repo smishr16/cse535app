29c1525b3661cad8a069255f6b6830d72bdef1b7
nattydreadlocks <smishr16@asu.edu>
2016-11-13 05:54:58 -0700

    Email & SMS Alerts
    In your build.gradle make sure to compile the jars added. Also make sure that you see in your libs folder.

--- app/src/main/java/asu/edu/cse535/locationawarereminder/services/LocationListenerService.java
+++ app/src/main/java/asu/edu/cse535/locationawarereminder/services/LocationListenerService.java
@@ -1,6 +1,7 @@
 package asu.edu.cse535.locationawarereminder.services;
 
 import android.Manifest;
+import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -14,16 +15,35 @@ import android.location.LocationManager;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.telephony.SmsManager;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import java.util.ArrayList;
 
 import asu.edu.cse535.locationawarereminder.R;
 import asu.edu.cse535.locationawarereminder.activities.NewTaskActivity;
+import asu.edu.cse535.locationawarereminder.database.Constants;
 import asu.edu.cse535.locationawarereminder.database.DBManager;
+import asu.edu.cse535.locationawarereminder.database.Properties;
 import asu.edu.cse535.locationawarereminder.database.Task;
+import android.util.Log;
+
+
 
 /**
  * Created by Sooraj on 10/31/2016.
  */
 public class LocationListenerService extends Service {
+
+    final String TAG = "LAR";
 
     @Nullable
     @Override
@@ -74,6 +94,8 @@ public class LocationListenerService extends Service {
         @Override
         public void onReceive(Context context, Intent intent) {
             String key = LocationManager.KEY_PROXIMITY_ENTERING;
+            String phoneNumber;
+            String emailId;
             int task_id = intent.getExtras().getInt("taskId");
             String desc = intent.getExtras().getString("taskDesc");
             Boolean entering = intent.getBooleanExtra(key, false);
@@ -98,6 +120,21 @@ public class LocationListenerService extends Service {
                     builder.setSubText("Description : " + desc);   //API level 16
                     notificationManager.notify(task_id, builder.build());
 
+                    String message = "You have open reminders for this location" + "/n" +
+                            "Description: " + desc + "/n";
+
+                    //Check if phone number is there and if so send SMS
+                    phoneNumber = getPhoneNumber();
+                    if (!phoneNumber.equals("")) {
+                        sendSMS(phoneNumber, message);
+                    }
+
+                    //Check if email is there and if so send Email
+                    emailId = getEmail();
+                    if (!emailId.equals("")) {
+                        sendEmail(emailId, message);
+                    }
+
                     //PendingIntent pi = PendingIntent.getBroadcast(LocationListenerService.this, task_id, intent, 0);
                     //LocationManager locMgr = (LocationManager) getSystemService(LOCATION_SERVICE);
                     if (ActivityCompat.checkSelfPermission(LocationListenerService.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(LocationListenerService.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -107,6 +144,131 @@ public class LocationListenerService extends Service {
                 //locMgr.removeProximityAlert(removePi);
                 context.unregisterReceiver(this);
             }
+        }
+    }
+
+    private String getPhoneNumber(){
+        String phoneNumber = "";
+        String[] propertyArray = {Constants.propertyEmail, Constants.propertyPhone};
+        ArrayList<Properties> resultList = DBManager.getProperties(propertyArray);
+        for (Properties row : resultList) {
+            if (row.getName().equals(Constants.propertyPhone))
+                phoneNumber = row.getValue();
+        }
+        return phoneNumber;
+    }
+
+    private String getEmail(){
+        String email = "";
+        String[] propertyArray = {Constants.propertyEmail, Constants.propertyPhone};
+        ArrayList<Properties> resultList = DBManager.getProperties(propertyArray);
+        for (Properties row : resultList) {
+            if (row.getName().equals(Constants.propertyEmail))
+                email = row.getValue();
+        }
+        return email;
+    }
+
+
+    private void sendSMS(String phoneNumber, String message) {
+        String SENT = "SMS_SENT";
+        String DELIVERED = "SMS_DELIVERED";
+        BroadcastReceiver sendBroadcastReceiver;
+        BroadcastReceiver deliveryBroadcastReceiver;
+        PendingIntent deliveredPI;
+        PendingIntent sentPI;
+
+        sentPI= PendingIntent.getBroadcast(this, 0, new Intent(SENT), 0);
+
+
+        deliveredPI= PendingIntent.getBroadcast(this, 0, new Intent(DELIVERED),0);
+        //---SMS has been sent---
+        sendBroadcastReceiver= new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        Log.v(TAG, "SMS sent");
+                        //Toast.makeText(getBaseContext(), "SMS Sent",
+                                //Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Log.v(TAG, "Generic failure");
+                        //Toast.makeText(getBaseContext(), "Generic Failure",
+                                //Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Log.v(TAG, "No Service");
+                        //Toast.makeText(getBaseContext(), "No Service",
+                                //Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Log.v(TAG, "Null PDU");
+                        //Toast.makeText(getBaseContext(), "Null PDU",
+                                //Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Log.v(TAG, "Radio Off");
+                        //Toast.makeText(getBaseContext(), "Radio Off",
+                                //Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        };
+        registerReceiver(sendBroadcastReceiver, new IntentFilter(SENT));
+        //---when the SMS has been delivered---
+        deliveryBroadcastReceiver= new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        Log.v(TAG, "SMS Delivered");
+                        //Toast.makeText(getBaseContext(), "SMS delivered",
+                                //Toast.LENGTH_SHORT).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Log.v(TAG, "SMS Not Delivered");
+                        //Toast.makeText(getBaseContext(), "SMS Not Delivered",
+                                //Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        };
+        registerReceiver(deliveryBroadcastReceiver, new IntentFilter(DELIVERED));
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);
+        unregisterReceiver(sendBroadcastReceiver);
+        unregisterReceiver(deliveryBroadcastReceiver);
+    }
+
+    private void sendEmail(String recipient, String textMessage){
+        String subject = "Alert from LAR App";
+        java.util.Properties properties = new java.util.Properties();
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.socketFactory.port", "465");
+        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.port", "465");
+
+        Session session = Session.getDefaultInstance(properties,  new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("notificationcse535app@gmail.com", "cse535app");
+            }
+        });
+        try{
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("notificationcse535app@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
+            message.setSubject(subject);
+            message.setContent(textMessage, "text/html; charset=utf-8");
+            Transport.send(message);
+            Log.v(TAG, "Email Delivered");
+        } catch(MessagingException e) {
+            e.printStackTrace();
+            Log.v(TAG, "Email Not Delivered");
+        } catch(Exception e) {
+            e.printStackTrace();
+            Log.v(TAG, "Email Not Delivered");
         }
     }
 }
